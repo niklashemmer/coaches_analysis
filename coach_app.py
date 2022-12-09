2817462328bbd5925153d2c52e4727d0a6f6efa2
Niklas Hemmer <niklas.hemmer@web.de>
2022-12-09 08:15:33 +0100

    first commit

---
+++ coach_app.py
@@ -0,0 +1,173 @@
+import streamlit as st
+import plotly.express as px
+import pandas as pd
+pd.set_option('display.max_columns', None)
+
+###################### LOAD IN THE DATA ##########################
+
+df = pd.read_csv("/Users/niklashemmer/PycharmProjects/coaches_analysis/data/streamlit.csv", index_col=None)
+df_summary = pd.read_csv("/Users/niklashemmer/PycharmProjects/coaches_analysis/data/coaches_summary.csv", index_col=None)
+big_five = ["FRA-Ligue 1", "ESP-La Liga", "GER-Bundesliga", "ITA-Serie A", "ENG-Premier League"]
+
+###################### CREATE STREAMLIT APP ##########################
+
+st.set_page_config(page_title="Plus/Minus Rating of Coaches", layout="wide")
+
+# --- SIDEBAR ---
+st.sidebar.header("Please Filter here:")
+
+league = st.sidebar.multiselect(
+    "Select League:",
+    options=df_summary["League"].unique(),
+    default=big_five
+)
+
+season = st.sidebar.multiselect(
+    "Select Season:",
+    options=df_summary["Season"].sort_values().unique(),
+    default=df_summary["Season"].sort_values().unique()
+)
+
+df_summary_selection = df_summary.query(
+    "League == @league & Season == @season"
+)
+
+# --- HEADER ---
+
+# Set header and add explanation
+st.title("Plus/Minus Rating of Coaches")
+st.write("This tool compares the actual points of clubs to point predictions of FiveThirtyEight at the start of the season. Over- or underperformance is ascribed to the coaches. The data goes back to 2018/2019 and includes multiple leagues across Europe. The seasons and leagues can be filtered on the left side.") #The data table below shows coaches and their achievements per season. By checking the box, one can analyze coaches on an aggregated level of the past seasons. The chart at the bottom displays updated expectations over the course of one season.")
+st.markdown('''##### <span style="color:black">Evaluate football coaches based on their over-/underperformance of expected points</span>
+            ''', unsafe_allow_html=True)
+#tab_coach, tab_season = st.tabs(["Coach total", "Coach per Season"])
+
+
+# --- DATA TABLE ---
+
+# Enable to aggregate based on coaches' career performance
+coach_level = st.checkbox("Aggregated numbers (Check if you want to analyze coaches over their career instead of individual seasons)")
+
+# Display an interactive table
+if coach_level:
+    df_summary_selection = df_summary_selection.groupby(["Name"]).agg({
+        "Matches": "sum",
+        "Expectation":"sum",
+        "Result":"sum",
+        "Difference":"sum"
+    }).sort_values(by="Difference", ascending=False).reset_index()
+else:
+    df_summary_selection = df_summary_selection.sort_values(by="Difference", ascending=False).reset_index(drop=True)
+
+st.dataframe(df_summary_selection.style.format(subset=['Difference', 'Expectation', 'Result'], formatter="{:.1f}"))
+
+# Add space
+st.write("##")
+
+
+# --- SCATTER PLOT ---
+
+# Enable to aggregate based on coaches' career performance
+coach_scatter = st.checkbox("Scatterplot with aggregated numbers (Check if you want to analyze coaches over their career instead of individual seasons)")
+
+if coach_scatter:
+    df_scatter = df_summary_selection.groupby(["Name"]).agg({
+        "Matches": "sum",
+        "Expectation":"sum",
+        "Result":"sum",
+        "Difference":"sum"
+    }).sort_values(by="Difference", ascending=False).reset_index()
+else:
+    df_scatter = df_summary_selection
+
+x = df_scatter["Matches"]
+y = df_scatter["Difference"].round(1)
+fig = px.scatter(
+    df_scatter,
+    x=x,
+    y=y,
+    labels = {
+        "Matches": "Number of matches",
+        "y": "Over-/Underachievement",
+        "Coach_ID":"Coach_ID"
+    },
+    height=600,
+    #title="<b>Over-/underachievement of expected points per coach</b>",
+    template="plotly_white",
+    hover_data=['Name']
+)
+
+config = {'displayModeBar': False}
+
+# Add red and green rectangle do highlight over-/underperformance
+fig.add_hrect(y0=0, y1=df_scatter["Difference"].min()-2, line_width=0, fillcolor="red", opacity=0.1)
+fig.add_hrect(y0=0, y1=df_scatter["Difference"].max()+2, line_width=0, fillcolor="green", opacity=0.1)
+
+fig.add_hline(y=0)
+
+fig.update_traces(marker_size=10, marker_color="#004CFF", marker_line_color="black")
+
+fig.update_layout(
+    plot_bgcolor="rgba(0,0,0,0)",
+    xaxis=dict(
+        range=[0, x.max()+1]
+    )
+)
+
+st.plotly_chart(fig, use_container_width=True, use_container_height=True)
+
+st.markdown("---")
+
+
+# --- PERFORMANCE LINE CHART ---
+
+st.markdown('''##### <span style="color:black">Tracking Performance: Updated expected points over a season</span>
+            ''', unsafe_allow_html=True)
+
+# Filter for coaches with at least 10 games per season
+df_10 = df.groupby("Coach_ID").agg({"Matches":"count"}).reset_index()
+df_10 = df_10[df_10["Matches"] >= 10]
+limit = df_10["Coach_ID"].tolist()
+df_player_select = df[df["Coach_ID"].isin(limit)]
+
+# Select a coach to be displayed
+player = st.selectbox("Choose a Coach_ID: Coach-Club-Season", df_player_select["Coach_ID"].unique())
+
+df_coach = df.query(
+    "Coach_ID == @player"
+).reset_index()
+
+x2 = df_coach["Matches"]
+y2 = df_coach["Points"]
+constant = df_coach["Points"].iloc[0]
+
+fig2 = px.line(
+    x=x2,
+    y=y2,
+    labels = {
+        "Matches": "Date",
+        "Points": "Updated expected points",
+        "Coach_ID":"Coach_ID",
+        "x":"Date",
+        "y":"Updated expected points"
+    },
+    markers=True
+)
+
+# Update layout
+fig2.update_layout(
+    plot_bgcolor="rgba(0,0,0,0)",
+    yaxis=dict(
+        range=[constant-20, constant+20],
+        tickmode="auto"
+    ),
+)
+
+fig2.update_traces(line=dict(color="black"))
+
+# Add horizontal line to illustrate expectations at the beginning
+fig2.add_hline(y=constant,
+               annotation_text="Expected points\nat season start",
+               annotation_position="bottom right",
+               line_width=0.5)
+
+st.plotly_chart(fig2, use_container_width=True, use_container_height=True)
